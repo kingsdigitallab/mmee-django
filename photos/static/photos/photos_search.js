cfd5188c094f99c897596158d450dd6cb59e2a65
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2024-11-28 23:58:16 +0000

    static: externalised the static api class. Use it in the search map to get info about a photo when a user click a marker.

--- photos/static/photos/photos_search.js
+++ photos/static/photos/photos_search.js
@@ -1,60 +1,3 @@
-class StaticAPI {
-    constructor() {
-        this.response = API_PHOTOS
-        // console.log(staticAPI.get_photo_urls())
-
-        // turn url to photos relative
-        this.response.data.forEach(photo => {
-            photo.attributes.image = photo.attributes.image.replace('src="/media/', 'src="../media/')
-            photo.attributes.url = photo.attributes.url.replace('/photos/', '')
-        })
-    }
-    get_photo_urls() {
-        return this.response.data.map(p => {
-            return `<a href="${p.id}/">${p.id}</a><br>`
-        }).join('\n')
-    }
-    filter(query) {
-        // .page: 1
-        // .perpage: 12
-        console.log(query)
-        // let ret = JSON.parse(JSON.stringify(this.response).replaceAll('height-375', 'height-700'));
-        let ret = JSON.parse(JSON.stringify(this.response));
-        ret.meta.query.view = query.view || 'grid';
-        ret.meta.query.perpage = parseInt(query.perpage || 12);
-        ret.meta.query.page = parseInt(query.page || 1);
-        let start = (ret.meta.query.page - 1) * ret.meta.query.perpage
-        ret.data = ret.data.slice(
-            start, 
-            start + ret.meta.query.perpage
-        )
-        ret.meta.qs = (new URLSearchParams(ret.meta.query)).toString()
-        ret.meta.pagination.per_page = ret.meta.query.perpage
-        ret.meta.pagination.page = ret.meta.query.page
-        ret.meta.pagination.pages = Math.ceil(ret.meta.pagination.count / ret.meta.pagination.per_page)
-
-        let q = {
-            ...ret.meta.query,
-            page: ret.meta.pagination.page + 1
-        }
-        if (q.page <= ret.meta.pagination.pages) {
-            ret.links.next = '?' + (new URLSearchParams(q)).toString()
-        }
-        q.page = ret.meta.pagination.page - 1
-        if (q.page > 0) {
-            ret.links.prev = '?' + (new URLSearchParams(q)).toString()
-        }
-        q.page = ret.meta.pagination.pages
-        ret.links.last = '?' + (new URLSearchParams(q)).toString()
-        q.page = 1
-        ret.links.first = '?' + (new URLSearchParams(q)).toString()
-
-        return ret
-    }
-}
-
-var staticAPI = new StaticAPI();
-
 var photo_search = function(g_initial_query) {
     /*
     notations:
@@ -252,7 +195,8 @@ var photo_search = function(g_initial_query) {
                     );
                 }
 
-                query.imgspecs = this.get_image_spec(3, g_image_size_increment);
+                // query.imgspecs = this.get_image_spec(3, g_image_size_increment);
+                query.imgspecs = 'height-375';
 
                 // request for the photo result (list of images)
                 let res = staticAPI.filter(query)
---
+++ photos/static/photos/static_api.js
@@ -0,0 +1,65 @@
+class StaticAPI {
+    constructor() {
+        this.response = API_PHOTOS
+        // console.log(staticAPI.get_photo_urls())
+
+        // turn url to photos relative
+        this.response.data.forEach(photo => {
+            photo.attributes.image = photo.attributes.image.replace('src="/media/', 'src="../media/')
+            photo.attributes.url = photo.attributes.url.replace('/photos/', '')
+        })
+    }
+    get_photo_urls() {
+        return this.response.data.map(p => {
+            return `<a href="${p.id}/">${p.id}</a><br>`
+        }).join('\n')
+    }
+    filter(query) {
+        // .page: 1
+        // .perpage: 12
+        console.log(query)
+        // let ret = JSON.parse(JSON.stringify(this.response).replaceAll('height-375', 'height-700'));
+        let resStr = JSON.stringify(this.response)
+        if (query.imgspecs) {
+            resStr = resStr.replaceAll('height-375', query.imgspecs)
+        }
+        let ret = JSON.parse(resStr);
+
+        ret.meta.query.view = query.view || 'grid';
+        ret.meta.query.perpage = parseInt(query.perpage || 12);
+        ret.meta.query.page = parseInt(query.page || 1);
+        let start = (ret.meta.query.page - 1) * ret.meta.query.perpage
+        ret.data = ret.data.slice(
+            start, 
+            start + ret.meta.query.perpage
+        )
+        ret.meta.qs = (new URLSearchParams(ret.meta.query)).toString()
+        ret.meta.pagination.per_page = ret.meta.query.perpage
+        ret.meta.pagination.page = ret.meta.query.page
+        ret.meta.pagination.pages = Math.ceil(ret.meta.pagination.count / ret.meta.pagination.per_page)
+
+        let q = {
+            ...ret.meta.query,
+            page: ret.meta.pagination.page + 1
+        }
+        if (q.page <= ret.meta.pagination.pages) {
+            ret.links.next = '?' + (new URLSearchParams(q)).toString()
+        }
+        q.page = ret.meta.pagination.page - 1
+        if (q.page > 0) {
+            ret.links.prev = '?' + (new URLSearchParams(q)).toString()
+        }
+        q.page = ret.meta.pagination.pages
+        ret.links.last = '?' + (new URLSearchParams(q)).toString()
+        q.page = 1
+        ret.links.first = '?' + (new URLSearchParams(q)).toString()
+
+        if (query.id) {
+            ret.data = ret.data.filter(p => p.id == query.id)
+        }
+
+        return ret
+    }
+}
+
+var staticAPI = new StaticAPI();
